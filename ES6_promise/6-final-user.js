bd2ec41b3a80958c036275a31f5eba93cb0573da
Modi-01 <moudhi.a.almutlaq@gmail.com>
2026-05-12 15:21:58 +0000

    Add Task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,27 +1,13 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const photoPromise = uploadPhoto(fileName);
   const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([photoPromise, userPromise])
-    .then((results) =>
-      results.map((result) => {
-        if (result.status === 'fulfilled') {
-          return {
-            status: 'fulfilled',
-            value: result.value,
-          };
-        }
-
-       
-        return {
-          status: 'rejected',
-          value: result.reason instanceof Error
-            ? result.reason.message  
-            : String(result.reason),
-        };
-      })
-    );
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
